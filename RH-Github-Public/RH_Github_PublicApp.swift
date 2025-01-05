63338c81d383123dc11fe81523c44e9349f059b0
Md Refat Hossain <refatibnanwar@gmail.com>
2025-01-05 19:58:42 +0800

    Initial Commit

---
+++ RH-Github-Public/RH_Github_PublicApp.swift
@@ -0,0 +1,17 @@
+//
+//  RH_Github_PublicApp.swift
+//  RH-Github-Public
+//
+//  Created by Md Refat Hossain on 05/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RH_Github_PublicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
